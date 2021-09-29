75f589587a5bdbaa98c0d970e5eaf661c0c4aa04
kirin <kirin_chen@morrisonexpress.com>
2021-09-30 00:51:07 +0800

    init commit

---
+++ fin-exchange-manage/main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Alt+Shift+X to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+Shift+B to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# See http://docs.jinkan.org/docs/flask/patterns/sqlalchemy.html
+
+# See https://www.maxlist.xyz/2019/11/10/flask-sqlalchemy-setting/
