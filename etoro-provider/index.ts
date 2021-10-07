3c5f2f072a2073a7a43090d81b54b6c4e106d3f5
Botman2000 <kirin.chen1985@gmail.com>
2021-10-07 22:00:04 +0800

    etoro

---
+++ etoro-provider/index.ts
@@ -0,0 +1,7 @@
+export function showTotal(first: number, second: number): number {
+    const total: number = first + second;
+    console.log(`Theaaaa total is: ${total}`);
+    return total;
+  }
+  
+  showTotal(100, 200);
