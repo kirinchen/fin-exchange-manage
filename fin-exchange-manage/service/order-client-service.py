578b0aad85b474dd43dcff8f8fd9989f51029a4a
Botman2000 <kirin.chen1985@gmail.com>
2021-10-16 10:42:55 +0800

    todo order-client-service.py

--- fin-exchange-manage/service/order-client-service.py
+++ fin-exchange-manage/service/order-client-service.py
@@ -1,10 +1,22 @@
 from abc import ABC
 from typing import List
 
+from dto.order_dto import OrderDto
+from utils import order_utils
 from utils.order_utils import OrderFilter, OrdersInfo
 
 
 class OrderClientService(ABC):
 
-    def query_order(self,exchange:str,filter:OrderFilter) -> OrdersInfo:
-        pass
+    def __init__(self, exchange: str):
+        self.exchange = exchange
+
+    def query_order(self, filter_obj: OrderFilter) -> OrdersInfo:
+        orders = self.list_all_order(symbol=filter_obj.symbol, limit=filter_obj.limit,
+                                     startTime=filter_obj.updateStartTime, endTime=filter_obj.updateEndTime)
+        return order_utils.filter_order(oods=orders, ft=filter_obj)
+
+    def list_all_order(self, symbol: str, orderId: int = None, startTime: int = None,
+                       endTime: int = None, limit: int = None) -> List[OrderDto]:
+        raise NotImplementedError('not impl')
+ 
