9bd70d0c8880acd9d5b42d5ff74d08ebc735bc1e
kirin <kirin.chen1001@gmail.com>
2021-12-08 11:14:12 +0800

    add default fuse tag

--- fin-exchange-manage/rest/position/fuse/post.py
+++ fin-exchange-manage/rest/position/fuse/post.py
@@ -1,5 +1,6 @@
 import exchange
 from infra import database
+from rest.position.fuse import DEFAULT_TAG
 from rest.proxy_controller import PayloadReqKey
 from service.position_fuse.mediation import StopMediation, StopMediationDto
 from utils import comm_utils
@@ -10,5 +11,6 @@ def run(payload: dict) -> dict:
         stopMediation: StopMediation = exchange.gen_impl_obj(exchange_name=PayloadReqKey.exchange.get_val(payload),
                                                              clazz=StopMediation, session=session)
         dto = StopMediationDto(**payload)
+        dto.tags.append(DEFAULT_TAG)
         stopMediation.init(dto)
         return comm_utils.to_dict(stopMediation.stop())
