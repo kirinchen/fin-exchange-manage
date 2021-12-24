80ee2185404bccba4b1cb7e8411124cc9c83eab3
kirin <kirin.chen1001@gmail.com>
2021-12-24 19:02:02 +0800

    add new method clean_orders.py

---
+++ fin-exchange-manage/rest/order/clean_orders.py
@@ -0,0 +1,29 @@
+from typing import List
+
+import exchange
+from dto.order_dto import OrderDto
+from infra import database
+
+from rest.proxy_controller import PayloadReqKey
+from service.order_client_service import OrderClientService
+from utils import comm_utils
+from utils.order_utils import OrderFilter
+
+
+class CleanOdsDto:
+
+    def __init__(self, symbol: str, currentOds: List[dict], **kwargs):
+        self.symbol = symbol
+        self.currentOds = currentOds
+
+    def get_current_ods_dto(self) -> List[OrderDto]:
+        return [OrderDto(**d) for d in self.currentOds]
+
+
+def run(payload: dict) -> dict:
+    with database.session_scope() as session:
+        order_client: OrderClientService = exchange.gen_impl_obj(exchange_name=PayloadReqKey.exchange.get_val(payload),
+                                                                 clazz=OrderClientService, session=session)
+        clean_ods_dto = CleanOdsDto(**payload)
+        result = order_client.clean_orders(clean_ods_dto.symbol, clean_ods_dto.get_current_ods_dto())
+        return comm_utils.to_dict(result)
