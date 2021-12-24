e51e0b144dc2e7c9dd5e8d0c4cce04c91dd3d904
kirin <kirin.chen1001@gmail.com>
2021-12-24 10:33:01 +0800

    add turn off debug env

--- fin-exchange-manage/main.py
+++ fin-exchange-manage/main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+import config
 import exchange
 from infra import database
 from model import init_data
@@ -20,12 +21,12 @@ exchange.load_all_service()
 sync_cron.init_bind_all()
 
 if __name__ == "__main__":
-
     # with database.session_scope() as session:
     #     p_dao: ProductDao = exchange.gen_impl_obj('binance', ProductDao,session)
     #     p_e = p_dao.get_by_item_symbol('BTC', 'USDT')
     #     print(p_e)
-    app.run(host='0.0.0.0', debug=True, port=9282, threaded=True)
+
+    app.run(host='0.0.0.0', debug=config.env_bool('debug'), port=9282, threaded=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
