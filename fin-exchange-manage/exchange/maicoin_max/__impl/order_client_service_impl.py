1891a118e9981b614701ddf9fc5e50a2d817bf85
kirin <kirin.chen1001@gmail.com>
2022-03-01 13:21:39 +0800

    todo stop order for max

--- fin-exchange-manage/exchange/maicoin_max/__impl/order_client_service_impl.py
+++ fin-exchange-manage/exchange/maicoin_max/__impl/order_client_service_impl.py
@@ -44,7 +44,21 @@ class MaxOrderClientService(OrderClientService):
 
     def post_stop_market(self, prd_name: str, price: float, quantity: float, positionSide: str,
                          tags: List[str]) -> OrderDto:
-        pass  # TODO
+        product = self.productDao.get_by_prd_name(prd_name)
+        pair = max_utils.unfix_symbol(prd_name)
+        m_info = MarketInfo(**product.get_config())
+        amt = max_utils.fix_amount(m_info=m_info, amt=quantity, curPrice=price)
+        if amt <= 0:
+            return None
+        side = 'buy' if positionSide == PositionSide.LONG else 'sell'
+        fixed_price = comm_utils.fix_precision(m_info.quote_unit_precision, price)
+        order_type: str =  'limit'
+        client_uid = comm_utils.get_order_cid(tags=tags)
+
+        o = self.client.set_private_create_order(pair=pair, side=side, amount=amt,
+                                                 price=fixed_price,
+                                                 _type=order_type, client_id=client_uid)
+        return max_utils.convert_order_dto(o)
 
     def post_take_profit(self, prd_name: str, price: float, quantity: float, positionSide: str,
                          tags: List[str]) -> OrderDto:
