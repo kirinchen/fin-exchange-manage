beb798b31760937667c8dded98ef4b81fa1a039c
kirin <kirin.chen1001@gmail.com>
2022-03-07 20:35:06 +0800

    change to smae bro

--- fin-exchange-manage/cron/cron_settings.py
+++ fin-exchange-manage/cron/cron_settings.py
@@ -16,5 +16,5 @@ def start_all():
     print(__file__ + ' start_all')
     scheduler.add_job(func=bzk_flow_off_restart_job.check, trigger="interval", seconds=2 * 60)
     scheduler.add_job(func=lend_funding_job.lend, trigger="interval", seconds=25 * 60)
-    scheduler.add_job(func=lend_funding_job_tom.lend, trigger="interval", seconds=37 * 60)
+    scheduler.add_job(func=lend_funding_job_tom.lend, trigger="interval", seconds=25 * 60)
     scheduler.start()
