7ba31bad0f067adcebc90ff6b9180c3f65ae176b
kirinchen <kirin.chen1001@gmail.com>
2022-09-10 10:51:28 +0800

    Update lend_funding_job.py

--- fin-exchange-manage/cron/lend_funding_job.py
+++ fin-exchange-manage/cron/lend_funding_job.py
@@ -31,8 +31,8 @@ lend_by_filter_usdt_payload = {
     "name": "wallet/lend_by_filter",
     "symbol": "UST",
     "rowAmount": 160,
-    "minMaxDiffRate": 0.286,
-    "middleWeight": 0.4,
+    "minMaxDiffRate": 0.586,
+    "middleWeight": 0.75,
     "wallet_type": "funding",
     "exchange": "bitfinex"
 }
